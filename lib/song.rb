c3267bf068e66f6e13227b2146a5f9e4df73b169
github username <github email address>
2018-08-29 14:58:56 -0400

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,52 @@
+class Song
+
+attr_reader :name, :artist, :genre
+
+@@count = 0
+@@artists = []
+@@genres = []
+
+def initialize(name, artist, genre)
+  @name = name
+  @artist = artist
+  @genre = genre
+
+  @@count += 1
+  @@artists << artist
+  @@genres << genre
+
+end
+
+def self.count
+  @@count
+end
+
+def self.artists
+  @@artists.uniq
+end
+
+def self.genres
+  @@genres.uniq
+end
+
+def self.genre_count
+  #returns hash of genres and the number of songs with those genres
+  @hash = {}
+  self.genres.each do |genre|
+    @hash[genre] = @@genres.count(genre)
+  end
+
+  @hash
+end
+
+def self.artist_count
+  #returns hash of genres and the number of songs with those genres
+  @hash = {}
+  self.artists.each do |artist|
+    @hash[artist] = @@artists.count(artist)
+  end
+
+  @hash
+end
+
+end
